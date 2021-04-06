85e6bf7d8417ce5f8e0fb21d879e323e4cc8ce50
Cxxshyy <djjames.catlyn@gmail.com>
2021-04-06 20:04:27 +0100

    init

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-
-createApp(App).use(store).use(router).mount('#app')
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import "./assets/@fortawesome/fontawesome-free/css/all.min.css";
+import "./assets/css/tailwind.css";
+createApp(App)
+  .use(store)
+  .use(router)
+  .mount("#app");
