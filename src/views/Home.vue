48f9e486a17099689a7743d02c35d97e2a41e41d
Cxxshyy <djjames.catlyn@gmail.com>
2021-04-07 00:25:28 +0100

    finished

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,25 @@
 <template>
   <div class="home">
-    <div class="text-white">Home</div>
+    <Tweet 
+      v-for="tweet in tweets" 
+      :key="tweet._id" 
+      :tweet="tweet" />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-
+import { ref } from 'vue';
+import Tweet from '../components/Tweet';
+import vweets from '../vweets';
 
 export default {
+  components: { Tweet },
+  setup () {
+    const tweets = ref(vweets);
 
+    return {
+      tweets,
+    }
+  }
 }
 </script>
